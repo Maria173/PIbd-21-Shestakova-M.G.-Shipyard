c822ae8d1fb628fb181f9aaefb5f163e35fe8c10
Mariia Shestakova <marijashestakova73@gmail.com>
2022-04-07 18:51:54 +0400

    5base

--- ShipyardContracts/BindingModels/OrderBindingModel.cs
+++ ShipyardContracts/BindingModels/OrderBindingModel.cs
@@ -8,6 +8,8 @@ namespace ShipyardContracts.BindingModels
         public int? Id { get; set; }
 
         public int ShipId { get; set; }
+        public int CarId { get; set; }
+        public int? ClientId { get; set; }
 
         public int Count { get; set; }
 
